835a27c88b5a9a4528c171ee8ae183b7d181e65b
Sampada Kulkarni <88284742+samkul-swe@users.noreply.github.com>
2025-03-26 19:34:14 -0700

    Time: 25 ms (5.02%), Space: 45.4 MB (28.98%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (magazine.contains(ransomNote)) {
+            return true;
+        } else {
+            Map<Character,Integer> map = new HashMap<>();
+            for (int i = 0; i < ransomNote.length(); i ++) {
+                if (map.containsKey(ransomNote.charAt(i))) {
+                    map.put(ransomNote.charAt(i),map.get(ransomNote.charAt(i)) + 1);
+                } else {
+                    map.put(ransomNote.charAt(i),1);
+                }
+            }
+
+            System.out.println(map);
+
+            for (int i = 0; i < magazine.length(); i ++) {
+                if (map.containsKey(magazine.charAt(i))) {
+                    map.put(magazine.charAt(i),map.get(magazine.charAt(i)) - 1);
+                }
+            }
+
+            System.out.println(map);
+
+            for (Map.Entry<Character,Integer> entry : map.entrySet()) {
+                if (entry.getValue() <= 0) {
+                    // do nothing
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
