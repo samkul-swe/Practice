17e9cdc02a48cb6576c7ad5ce115bbafc29903b0
Sampada Kulkarni <88284742+samkul-swe@users.noreply.github.com>
2025-05-14 23:37:19 -0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The dice problem/the-dice-problem.java
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int N = sc.nextInt();
+
+            Solution ob = new Solution();
+            int ans = ob.oppositeFaceOfDice(N);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int oppositeFaceOfDice(int n) {
+        // code here
+        return Math.abs(7-n);
+    }
+}
