1e89b8dbf9aa68cd00d7549cc6e36e277994d71e
Sampada Kulkarni <88284742+samkul-swe@users.noreply.github.com>
2025-05-22 13:52:01 -0700

    Time: 6 ms (45.25%), Space: 43.3 MB (72.28%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,11 +1,20 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] sChars = s.toCharArray();
-        char[] tChars = t.toCharArray();
-        
-        Arrays.sort(sChars);
-        Arrays.sort(tChars);
-        
-        return Arrays.equals(sChars, tChars);
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i ++) {
+            count[s.charAt(i) - 'a'] ++;
+            count[t.charAt(i) - 'a'] --;
+        }
+
+        for (int val : count) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
