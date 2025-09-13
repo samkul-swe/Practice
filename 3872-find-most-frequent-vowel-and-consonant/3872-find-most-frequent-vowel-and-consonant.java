74018384199fc07d34a46c9a88cd2168d611d41f
Sampada Kulkarni <88284742+samkul-swe@users.noreply.github.com>
2025-09-12 19:42:34 -0700

    Time: 2 ms (66.22%), Space: 43.5 MB (8.94%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -1,32 +1,25 @@
 class Solution {
     public int maxFreqSum(String s) {
-        String vowels = "aeiou";
-        Map<Character,Integer> map = new HashMap<>();
-        int vowel = Integer.MIN_VALUE, consonant = Integer.MIN_VALUE;
-        for (int i = 0; i < s.length(); i++) {
-            if (map.containsKey(s.charAt(i))) {
-                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
-            } else {
-                map.put(s.charAt(i), 1);
-            }
+        int[] freq = new int[26];
+        for (int i = 0; i < 26; i++) {
+            freq[i] = 0;
+        }
+        for (int i = 0; i < s.length(); i ++) {
+            freq[s.charAt(i) - 'a'] ++;
         }
 
+        String vowels = "aeiou";
+        int maxVowel = 0;
+        int maxConsonant = 0;
         for (int i = 0; i < s.length(); i ++) {
             char c = s.charAt(i);
-            if ((vowels.contains(String.valueOf(c))) && (map.get(c) > vowel)) {
-                vowel = map.get(c);
-            } else if (!(vowels.contains(String.valueOf(c))) && (map.get(c) > consonant)){
-                consonant = map.get(c);
+            if (vowels.contains(String.valueOf(c))) {
+                maxVowel = Math.max(maxVowel, freq[c - 'a']);
+            } else {
+                maxConsonant = Math.max(maxConsonant, freq[c - 'a']);
             }
         }
 
-        if (consonant < 0) {
-            consonant = 0;
-        } else if (vowel < 0) {
-            vowel = 0;
-        }
-
-        return vowel + consonant;
-
+        return maxVowel + maxConsonant;
     }
 }
