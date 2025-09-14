e6b658504816fc5f0480f6092454f4e7cc716ec5
Sampada Kulkarni <88284742+samkul-swe@users.noreply.github.com>
2025-09-14 12:39:22 -0700

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/String stack/string-stack.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean stringStack(String pat, String tar) {
+        // code here
+        int i = pat.length() - 1 ;
+        int j = tar.length() - 1 ;
+       
+           while(i>=0 && j>=0)
+           {
+               if(pat.charAt(i) == tar.charAt(j))
+               {
+                   i--;
+                   j--;
+               }
+               else
+               {
+                   i = i-2;
+               }
+           }
+           
+           return j < 0;
+    }
+}
